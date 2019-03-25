717a55e982a103f2de1ebb2cb995e3d5bc2658e1
rmedina09 <raulmp@ciencias.unam.mx>
2019-03-25 15:36:56 -0600

    Update main_reinstalar_geoserver.py

--- main_reinstalar_geoserver.py
+++ main_reinstalar_geoserver.py
@@ -7,8 +7,7 @@ import time
 
 __author__="Raúl Medina Peña"
 
-# Funcion que elimina los workspaces, datastores, group layers, layers, y styles de un geoserver.
-#variables acceso del Geoserver
+#Variables acceso del Geoserver
 USUARIO        = 'admin'
 PASSW          = 'geoserver'
 URL_GEOSERVER  = 'http://localhost:8080/geoserver_try'
@@ -18,19 +17,19 @@ PATH_STYLES    = '/home/rmedina/RESPALDOS/respaldo_ACTUAL/ServerData/GeoserverCe
 
 #Funcion que limpia un geoserver, elimina los workspaces, stores, layers y styles por default.
 def clean_geoserver(user, pwd, url_geoserver):
-	#Variables para borrar workspaces
+	#Sentencia para obtener los workspaces por default para despues borrarlos
 	rest_get_workspaces = 'curl -u {usuario}:{password} -X GET {url}/rest/workspaces.json'.format(usuario=user, password=pwd, url=url_geoserver)
+	#Sentencia para eliminar los workspaces
 	prefix_rest_delete_ws = 'curl -u {usuario}:{password} -X DELETE {url}/rest/workspaces/'.format(usuario=user, password=pwd, url=url_geoserver)
+	#recurse=true, forzamos a que se ejecute la peticion
 	ext_recurse = '.json?recurse=true'
-	ext = '.json'
 
-	#Obtenemos todos los workspaces por default del geoserver
+	#Ejecutamos la peticion, para obtener todos los workspaces por default del geoserver
 	response = subprocess.check_output(rest_get_workspaces, shell=True)
+	#El resultado de la peticion lo convertimos en un JSON
 	ws_json = json.loads(response.decode())
 	
 	if ws_json['workspaces'] != '':
-
-		
 		#Borramos cada workspace 
 		for ws in ws_json['workspaces']['workspace']:
 			rest_delete = prefix_rest_delete_ws + ws['name'] + ext_recurse
@@ -38,36 +37,49 @@ def clean_geoserver(user, pwd, url_geoserver):
 			subprocess.call(rest_delete, shell=True)
 
 	#Variables para borrar styles
+	#Sentencia para obtener los estilos por default
 	rest_get_styles = 'curl -u {usuario}:{password} -X GET {url}/rest/styles.json'.format(usuario=user, password=pwd, url=url_geoserver)
+	#Sentencia para eliminar los estilos por default
 	prefix_rest_delete_styles = 'curl -u {usuario}:{password} -X DELETE {url}/rest/styles/'.format(usuario=user, password=pwd, url=url_geoserver)
 
-	#Obtenemos los estilos por default del geoserver
+	#Ejecutamos la peticion para obtener los estilos por default del geoserver
 	response_sld = subprocess.check_output(rest_get_styles, shell=True)
+	#Guardamos el resultado de la peticion en un JSON
 	sld_json = json.loads(response_sld.decode())
 	
 	if sld_json['styles'] != '':
+		#Iteramos sobre los estilos para ir eliminando uno por uno
 		for sld in sld_json['styles']['style']:
-			sld_delete = prefix_rest_delete_styles + sld['name'] + ext
+			sld_delete = prefix_rest_delete_styles + sld['name'] + ext_recurse
 			#print(sld_delete)
 			subprocess.call(sld_delete, shell=True)
 
 #Funcion que crea los workspaces a partir de la carpeta de un geoserver.
 def create_workspaces_geoserver(user, pwd, url_geoserver, path_resource):
+	#Sentencia que representa el xml para poder crear el workspace
 	workspace_xml = '<workspace><name>{name}</name></workspace>'
+	#Sentencia para crear un wrokspace
 	rest_workspace = 'curl -v -u {usuario}:{password} -XPOST -H "Content-type: text/xml" -d "{xml}" {url}/rest/workspaces'
+	#Obtenemos los directoriios que representan los workspace de nuestro repositorio local
 	workspaces = next(os.walk(path_resource))[1]
 	
+	#Creamos cada workspace ene l geoserver
 	for ws in workspaces:
 		ws_xml = workspace_xml.format(name=ws)
 		#print(ws_xml)
 		rest_post = rest_workspace.format(usuario=user, password=pwd, xml=ws_xml, url=url_geoserver)
-		print(rest_post)
+		#print(rest_post)
 		subprocess.call(rest_post, shell=True)
 
+#Funcion que carga las capas en el nuevo geoserver
 def put_layers_geoserver(user, pwd, url_geoserver, path_layers):
+	#Sentencia para poder crear layers en el geoserver
 	rest_layer = 'curl -v -u admin:geoserver -XPUT -H "Content-type: text/plain" -d "file://{path_layers}{store}/{shape}" \
 			      {url_geoserver}/rest/workspaces/cen/datastores/{store}/external.shp'
+	#Obtenemos los directorios en donde se encuentran los shapefile de nuestro repositorio local
 	stores = next(os.walk(path_layers))[1]
+	#Creamos cada layer en el geoserver 
+	#(NOTA: no se ha podido guardar la capa de fondo ya que es una piramide de mosaicos)
 	for st in stores:
 		shape = get_shapefile('{path_layers}{store}/'.format(path_layers=path_layers, store=st))
 		if shape is not None:
@@ -75,16 +87,21 @@ def put_layers_geoserver(user, pwd, url_geoserver, path_layers):
 			print(rest_put)
 			subprocess.call(rest_put, shell=True)
 
+#Funcion que pone los estilos en el geoserver
 def put_styles_geoserver(user, pwd, url_geoserver, path_styles):
+	#Sentencia que representa el archivo XML para crear un nuevo "style"
 	name_style     = '<style><name>{name}</name><filename>{name}.sld</filename></style>'
+	#Sentencia para crear el nuevo "style" en el geoserver
 	rest_style     = 'curl -v -u {usuario}:{password} -XPOST -H "Content-type: text/xml" -d "{archivo_xml}" \
 	                 {url_geoserver}/rest/workspaces/cen/styles'
+	#Sentencia para guardar el contenido del "style" 
 	rest_contenido = 'curl -v -u {usuario}:{password} -XPUT -H "Content-type: application/vnd.ogc.sld+xml" -d @{path}{name_archivo}.sld \
 	                 {url_geoserver}/rest/workspaces/cen/styles/{name_archivo}'
 	
 	#Obtenemos los nombres de los archivos .sld
 	styles = get_all_styles(path_styles)
 	
+	#Creamos cada "style" en el geoserver
 	for s in styles:
 		xml_style = name_style.format(name=s.rstrip(".sld"))
 		rest_post = rest_style.format(usuario=user, password=pwd, archivo_xml=xml_style, url_geoserver=url_geoserver)
@@ -96,12 +113,14 @@ def put_styles_geoserver(user, pwd, url_geoserver, path_styles):
 		subprocess.call(rest_put, shell=True)
 
 
+#Obtiene el archivo con extension .shp
 def get_shapefile(path_resource):
 	files = os.listdir(path_resource)
 	for f in files:
 		if f.endswith('.shp'):
 			return f
 
+#Obtiene todos los archivos .sld de un directorio
 def get_all_styles(path_resource):
 	styles = []
 	files = os.listdir(path_resource)
@@ -111,7 +130,7 @@ def get_all_styles(path_resource):
 	return styles
 
 
-
+#Firma del metodo MAIN del programa
 if __name__ == "__main__":
 	clean_geoserver(USUARIO, PASSW, URL_GEOSERVER)
 	create_workspaces_geoserver(USUARIO, PASSW, URL_GEOSERVER, PATH_WORKSPACE)
